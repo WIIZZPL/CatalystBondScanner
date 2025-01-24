97c5e90f34e8e892e8e26875731a451c3d2abd3f
WIIZZPL <WIIZZ_PL@outlook.com>
2025-01-24 15:41:34 +0100

    Async done TODO: scrapers

--- scraper/AsyncRateLimiter.py
+++ scraper/AsyncRateLimiter.py
@@ -1,22 +1,40 @@
+import asyncio
+import datetime
+from asyncio import Semaphore
 from configparser import ConfigParser
 from importlib import resources
 
 
 class AsyncRateLimiter:
-    _instance = None
 
-    def __init__(self):
-        if AsyncRateLimiter._instance is not None:
-            raise Exception("Singleton violation")
-        else:
-            AsyncRateLimiter._instance = self
-            self.config = ConfigParser()
-            config_file = resources.files(__name__).joinpath('config.ini')
-            self.config.read(config_file)
+    def __init__(self, domain :str = 'DEFAULT'):
+        self.config_parser = ConfigParser()
+        config_file = resources.files(__name__).joinpath('config.ini')
+        self.config_parser.read(config_file)
 
+        self.domain = domain
+        self.semaphore = None
+        self.timeout = None
+        self.last_request_datetime = datetime.datetime.min
 
-    @classmethod
-    def get_instance(cls):
-        if AsyncRateLimiter._instance is None:
-            AsyncRateLimiter()
-        return AsyncRateLimiter._instance
+        if domain in self.config_parser:
+            self.semaphore = Semaphore(int(self.config_parser[domain]['max_connections'])) if 'max_connections' in self.config_parser[domain] else None
+            self.timeout = datetime.timedelta(seconds=float(self.config_parser[domain]['timeout'])) if 'timeout' in self.config_parser[domain] else None
+
+        if self.semaphore is None:
+            self.semaphore = Semaphore(int(self.config_parser['DEFAULT']['max_connections']))
+        if self.timeout is None:
+            self.timeout = datetime.timedelta(seconds=float(self.config_parser['DEFAULT']['timeout']))
+
+    async def __aenter__(self):
+        await self.semaphore.acquire()
+
+        while True:
+            now = datetime.datetime.now()
+            delta = now-self.last_request_datetime
+            if delta > self.timeout:
+                return
+            await asyncio.sleep((self.timeout-delta).total_seconds())
+
+    async def __aexit__(self, exc_type, exc_val, exc_tb):
+        self.semaphore.release()
